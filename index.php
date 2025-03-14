1fea9a6ee10820bf104c6b92a44b176e88c3fea2
salmawafaaa <salma1905060@miuegypt.edu.eg>
2025-03-15 00:17:38 +0200

    update

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
+
+
 <?php
 require_once __DIR__ . '/Controller/OrderController.php';
 
-require_once './View/paymentView.php';
-
+$orderId = 1; // Assume order ID is 1 for testing
 $controller = new OrderController();
-$controller->checkout();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    echo "<pre>DEBUG: Form submitted!</pre>";
+    $paymentMethod = $_POST['payment_type'];
+    $controller->processPayment($orderId, $paymentMethod);
+}
+
 ?>
