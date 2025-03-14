16efd4bc363be1b2df163f6d030bf2b02fb29a7c
salmawafaaa <salma1905060@miuegypt.edu.eg>
2025-03-15 00:16:05 +0200

    update

--- Model/OrderModel.php
+++ Model/OrderModel.php
@@ -1,15 +1,26 @@
 <?php
 require_once 'PaymentStrategy.php';
+require_once "dbConnectionSingelton.php";
 
-class Order {
+
+class Order  {
     private $paymentMethod;
 
     public function __construct(PaymentStrategy $paymentMethod) {
         $this->paymentMethod = $paymentMethod;
+        //$this->db = Database::getInstance()->getConnection();
+        
+
     }
 
-    public function processPayment($amount) {
-        $this->paymentMethod->pay($amount);
+    public static function getTotalAmount($orderId) {
+        $db = DatabaseConnection::getInstance()->getConnection();
+        $stmt = $db->prepare("SELECT total FROM orders WHERE id = ?");
+        $stmt->bind_param("i", $orderId);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_assoc();
+        return $result ? $result['total'] : 0;
     }
 }
 ?>
+
