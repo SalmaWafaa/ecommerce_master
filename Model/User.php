6535e2315feea7cb02f889d4020c90e1aac0266d
saja <saja1904826@miuegypt.edu.eg>
2025-04-22 17:55:04 +0200

    somee

--- Model/User.php
+++ Model/User.php
@@ -1,64 +1,16 @@
 <?php
-
-// abstract class User {
-//     protected $id;
-//     protected $firstName;
-//     protected $lastName;
-//     protected $email;
-//     protected $password;
-
-//     public function __construct($id, $firstName, $lastName, $email, $password) {
-//         $this->id = $id;
-//         $this->firstName = $firstName;
-//         $this->lastName = $lastName;
-//         $this->email = $email;
-//         $this->password = $password;
-//     }
-
-//     // Setter for ID
-//     public function setId($id) {
-//         $this->id = $id;
-//     }
-
-//     // Getter for ID
-//     public function getId() {
-//         return $this->id;
-//     }
-
-//     // Setter for first name
-//     public function setFirstName($firstName) {
-//         $this->firstName = $firstName;
-//     }
-
-//     // Setter for last name
-//     public function setLastName($lastName) {
-//         $this->lastName = $lastName;
-//     }
-
-//     // Setter for email
-//     public function setEmail($email) {
-//         $this->email = $email;
-//     }
-
-//     // Setter for password
-//     public function setPassword($password) {
-//         $this->password = $password;
-//     }
-
-//     abstract public function login();
-//     abstract public function register();
-//     abstract public function editAccount();
-// }
-
 require_once __DIR__ . '/../config/dbConnectionSingelton.php';
-abstract class User {
+
+abstract class User 
+{
     protected $id;
     protected $firstName;
     protected $lastName;
     protected $email;
     protected $password;
 
-    public function __construct($id, $firstName, $lastName, $email, $password) {
+    public function __construct($id, $firstName, $lastName, $email, $password) 
+    {
         $this->id = $id;
         $this->firstName = $firstName;
         $this->lastName = $lastName;
@@ -119,43 +71,3 @@ abstract class User {
     abstract public function register();
     abstract public function editAccount();
 }
-// abstract class User {
-//     protected $id;
-//     protected $firstName;
-//     protected $lastName;
-//     protected $email;
-//     protected $password;
-//     protected $db;
-
-//     public function __construct($id, $firstName, $lastName, $email, $password) {
-//         $this->id = $id;
-//         $this->firstName = $firstName;
-//         $this->lastName = $lastName;
-//         $this->email = $email;
-//         $this->password = $password;
-        
-//         $database = Database::getInstance();
-//         $this->db = $database->getConnection();
-//     }
-
-//     // Common getters
-//     public function getId() { return $this->id; }
-//     public function getFirstName() { return $this->firstName; }
-//     public function getLastName() { return $this->lastName; }
-//     public function getEmail() { return $this->email; }
-
-//     // Common setters
-//     public function setFirstName($firstName) 
-//     { 
-//         $this->firstName = $firstName; 
-//     }
-//     public function setLastName($lastName) { $this->lastName = $lastName; }
-//     public function setEmail($email) { $this->email = $email; }
-//     public function setPassword($password) { $this->password = $password; }
-
-//     // Abstract methods each user type must implement
-//     abstract public function login();
-//     abstract public function register();
-//     abstract public function editAccount();
-// }
-?>
